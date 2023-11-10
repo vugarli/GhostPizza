d06b93db909dcc4b4990aaa59521f96d77c1d057
mb101royal <mi8cugw8y@code.edu.az>
2023-11-10 11:36:58 +0400

    Exceptions

---
+++ GhostPizza.InfraStructure/Exceptions/CredentialsAreInvalidException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostPizza.InfraStructure.Exceptions
+{
+	internal class CredentialsAreInvalidException : Exception
+	{
+		public CredentialsAreInvalidException()
+		{
+		}
+
+		public CredentialsAreInvalidException(string? message) : base(message)
+		{
+		}
+
+		public CredentialsAreInvalidException(string? message, Exception? innerException) : base(message, innerException)
+		{
+		}
+
+		protected CredentialsAreInvalidException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
---
+++ GhostPizza.InfraStructure/Exceptions/PizzaNotFoundException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhostPizza.InfraStructure.Exceptions
+{
+	internal class PizzaNotFoundException : Exception
+	{
+		public PizzaNotFoundException()
+		{
+		}
+
+		public PizzaNotFoundException(string? message) : base(message)
+		{
+		}
+
+		public PizzaNotFoundException(string? message, Exception? innerException) : base(message, innerException)
+		{
+		}
+
+		protected PizzaNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+		{
+		}
+	}
+}
